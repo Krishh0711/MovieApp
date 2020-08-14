5b4b71b70e7df0f5a524345210d46e4eded45ae5
Krishh258 <krishnanshu33@gmail.com>
2020-08-15 02:29:23 +0530

    add action creators

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
- import {data} from '../data';
+import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 
 class App extends React.Component {
@@ -17,10 +18,7 @@ class App extends React.Component {
 
     //make api call
     //dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    })
+    store.dispatch(addMovies(data));
 
     console.log(this.props.store.getState());
   }
