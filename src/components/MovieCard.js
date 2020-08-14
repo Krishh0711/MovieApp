d5f9a7b5059f0b83adeaebb2f0efe6b2689466a1
Krishh258 <krishnanshu33@gmail.com>
2020-08-15 03:57:58 +0530

    add and remove from favourites

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { addFavourite, removeFromFavourites } from '../actions';
 
 
 class MovieCard extends React.Component {
-  
+    
+    handleFavouriteClick = () => {
+        const {movie} = this.props;
+        this.props.dispatch(addFavourite(movie))
+    }
+
+    handleUnFavouriteClick = () => {
+        const { movie } = this.props;
+        this.props.dispatch(removeFromFavourites(movie));
+    }
+
     render() {
-    const {movie} = this.props;
+    const {movie, isFavourite} = this.props;
     return(
 
         <div className="movie-card">
@@ -16,7 +27,12 @@ class MovieCard extends React.Component {
             <div className="plot">{movie.Plot}</div>
             <div className="footer">
                 <div className="rating">{movie.imdbRating}</div>
-                <button className="favourite-btn">Favourite</button>
+                {
+                    isFavourite
+                    ? <button className="unfavourite-btn" onClick={this.handleUnFavouriteClick}>Unfavourite</button>
+                    : <button className="favourite-btn" onClick={this.handleFavouriteClick}>Favourite</button>   
+                }
+
             </div>
         </div>
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,42 @@
-import {ADD_MOVIES} from '../actions';
+import {ADD_MOVIES, ADD_FAVOURITE, REMOVE_FROM_FAVOURITES} from '../actions';
 
 const initialMoviesState = {
     list: [],
-    favoorites: []
+    favourites: []
 }
 export default function movies(state = initialMoviesState,action ) { 
-    if(action.type === ADD_MOVIES) {
-        return {
-            ...state,
-            list: action.movies
-        }
+    // if(action.type === ADD_MOVIES) {
+    //     return {
+    //         ...state,
+    //         list: action.movies
+    //     }
+    // }
+    // return state;
+
+    switch (action.type) {
+        case ADD_MOVIES:
+            return {
+                        ...state,
+                        list: action.movies
+            }
+        
+        case ADD_FAVOURITE:
+            return {
+                ...state,
+                favourites: [action.movie, ...state.favourites]
+            }
+            
+        case REMOVE_FROM_FAVOURITES:
+             const filteredArray = state.favourites.filter(
+              movie =>  movie.Title !== action.movie.Title
+             );
+             
+             return {
+                 ...state,
+                 favourites: filteredArray
+             }
+        default:
+           return state;     
     }
-    return state;
 }
 
